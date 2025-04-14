4181c6be39c1b3910c1ef4be441e4dca4a610216
mojyack <mojyack@gmail.com>
2025-04-14 11:58:42 +0900

    macro: undef before define

--- src/macro.hpp
+++ src/macro.hpp
@@ -1,12 +1,14 @@
 #pragma push_macro("ensure")
 #pragma push_macro("unwrap")
 
+#undef ensure
 #define ensure(var, ...)                               \
     if(!(var)) {                                       \
         __VA_OPT__(std::println(stderr, __VA_ARGS__)); \
         return false;                                  \
     }
 
+#undef unwrap
 #define unwrap(var, opt)    \
     auto var##_opt = (opt); \
     ensure(var##_opt);      \
